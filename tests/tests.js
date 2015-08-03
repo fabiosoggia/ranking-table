301628bfc41cfc6903556c4dcd22da37e56324ea
Fabio Soggia <f.soggia@gmail.com>
2015-08-03 23:09:01 +0200

    Unit tests extended.

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,93 @@
+QUnit.module("JsonUtils");
+QUnit.test("JsonUtils.cloneJson()", function(assert) {
+	assert.deepEqual([], [], "Clone empty array");
+	assert.deepEqual([1, 2], [1, 2], "Clone number array");
+	assert.deepEqual([{}], [{}], "Clone array with empty objects");
+	assert.deepEqual([{ a: "a" }], [{a: "a"}], "Clone array with not empty objects");
+	assert.deepEqual({}, {}, "Clone empty objects");
+	assert.deepEqual({ a: "a" }, { a: "a" }, "Clone not empty objects");
+	assert.deepEqual({ a: "a", b: [1, 2] }, { a: "a", b: [1, 2] }, "Clone not empty objects with arrays");
+});
+
+QUnit.module("RatingCalculator");
+QUnit.test("RatingCalculator.computeRatingDiff()", function(assert) {
+
+	assert.equal(
+			RatingCalculator.computeRatingDiff(10, 10, RatingCalculator.NEUTRAL_GROUND),
+			0,
+			"Ratings (10, 10, NEUTRAL_GROUND)"
+		);
+
+	assert.equal(
+			RatingCalculator.computeRatingDiff(10, 10, RatingCalculator.FIRST_TEAM_HOME),
+			3,
+			"Ratings (10, 10, FIRST_TEAM_HOME)"
+		);
+
+	assert.equal(
+			RatingCalculator.computeRatingDiff(10, 10, RatingCalculator.SECOND_TEAM_HOME),
+			3,
+			"Ratings (10, 10, SECOND_TEAM_HOME)"
+		);
+
+	assert.equal(
+			RatingCalculator.computeRatingDiff(10, 30, RatingCalculator.NEUTRAL_GROUND),
+			-10,
+			"Ratings diff can't be lower than -10"
+		);
+
+	assert.equal(
+			RatingCalculator.computeRatingDiff(30, 10, RatingCalculator.NEUTRAL_GROUND),
+			10,
+			"Ratings diff can't be greater than 10"
+		);
+});
+
+QUnit.test("RatingCalculator.computeGainedPoints()", function(assert) {
+
+	// Team 1
+
+	assert.equal(
+			RatingCalculator.computeGainedPoints(RatingCalculator.OUTCOME_DRAW, 10)[0],
+			1,
+			"(Team 1) Draw, each team loses or wins the points calculated by dividing the rating difference by 10"
+		);
+
+	assert.deepEqual(
+			RatingCalculator.computeGainedPoints(RatingCalculator.OUTCOME_TEAM_ONE_WIN, 10)[0],
+			1 - (10 / 10),
+			"(Team 1) If the first team wins, then it gains 1 - ( rating difference / 10 )"
+		);
+
+	assert.deepEqual(
+			RatingCalculator.computeGainedPoints(RatingCalculator.OUTCOME_TEAM_TWO_WIN, 10)[0],
+			-(1 + (10 / 10)),
+			"(Team 1) if the first team loses, then it loses 1 + ( rating difference / 10 )"
+		);
+
+
+	// Team 2
+
+	assert.equal(
+			RatingCalculator.computeGainedPoints(RatingCalculator.OUTCOME_DRAW, 10)[1],
+			10 / 10,
+			"(Team 2) Draw, each team loses or wins the points calculated by dividing the rating difference by 10"
+		);
+
+	assert.deepEqual(
+			RatingCalculator.computeGainedPoints(RatingCalculator.OUTCOME_TEAM_ONE_WIN, 10)[1],
+			-(1 - (10 / 10)),
+			"(Team 1) If the first team wins, the opposing team’s rating loses 1 - ( rating difference / 10 )"
+		);
+
+	assert.deepEqual(
+			RatingCalculator.computeGainedPoints(RatingCalculator.OUTCOME_TEAM_TWO_WIN, 10)[0],
+			(1 + (10 / 10)),
+			"(Team 1) if the first team loses, then it loses 1 + ( rating difference / 10 )"
+		);
+
+});
+
 QUnit.module("PULSEBALL");
 QUnit.test("PULSEBALL.addMatch()", function(assert) {
 
